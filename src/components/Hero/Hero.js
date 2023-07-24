b221912ff203384a023d6426a9d86d73cc9e81e6
cybraia <27krishna2002@gmail.com>
2023-07-24 18:09:55 +0530

    summary

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => (
         Krishna Mehta
       </SectionTitle>
       <SectionText>
-        I am a Computer Science student at the PES University. I am passionate about software development and am currently looking for summer internship opportunities for Summer 2023.
+        I am a Computer Science senior at the PES University. I am passionate about software development and am currently looking for spring internship opportunities for 2024 and full time opportunities.
       </SectionText>
       <Button><a href='https://drive.google.com/file/d/1Xee5wEGkFDCupuh4kn_e0XTjQpl1-TFW/view?usp=sharing' style={{textDecoration: "none", color: "white"}}>Resume</a></Button>
     </LeftSection>
